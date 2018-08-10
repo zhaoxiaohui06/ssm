41aef71ab6a9d49819481a0148a02bf5e51a67a3
zhaoxiaohui <zhaoxiaohui06@163.com>
2018-08-10 17:18:25 +0800

    @Autowired

--- src/main/java/com/how2java/controller/CategoryController.java
+++ src/main/java/com/how2java/controller/CategoryController.java
@@ -4,7 +4,6 @@ package com.how2java.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
